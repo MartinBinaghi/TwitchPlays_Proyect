ca9667cb0e75bfcb13edac1b1f49f1f672119e7d
MartinBinaghi <martin.binaghi01@gmail.com>
2023-03-16 21:11:00 -0300

    update

---
+++ promedio.hs
@@ -0,0 +1,14 @@
+promedio:: Float -> Float -> Float
+promedio x y = (x+y)/2
+
+f:: Float -> Float
+f x = 5 * x
+
+duplica :: Float -> Float
+duplica a = a + a
+
+por2:: Float -> Float
+por2 y = 2 * y
+
+multiplicar :: Float -> Float -> Float
+multiplicar zz tt = zz * tt
