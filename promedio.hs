31fb5ce8404875163b54677a8d047083d72393aa
MartinBinaghi <martin.binaghi01@gmail.com>
2023-03-24 19:38:15 -0300

    update

--- promedio.hs
+++ promedio.hs
@@ -27,3 +27,16 @@ rangoPrecio precio | precio <= 0 = "esto no puede ser"
                    | 0 < precio && precio <2000 = "muy barato"
                    | 2000 <= precio && precio <= 5000 = "hay que ver bien"
                    | precio > 5000 = "demasiado caro"
+
+esMultiploDe :: Int -> Int -> Bool
+esMultiploDe numero divisor | mod numero divisor== 0 = True
+                            | otherwise = False
+
+max3::Int-> Int ->Int ->Int 
+max3 x y z = max (max x y) z
+
+min3::Int-> Int ->Int ->Int 
+min3 x y z = min (min x y) z
+
+dispersion:: Int-> Int ->Int ->Int 
+dispersion x y z = (max3 x y z) - (min3 x y z)
